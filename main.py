9c0d498a1758fee78798677b2db1d932fabd48ec
Erik Revaj <erik.revaj3@gmail.com>
2020-11-04 18:55:07 +0100

    union and intersections

--- main.py
+++ main.py
@@ -1,18 +1,41 @@
-farm_animals = {"sheep", "cow", "hen"}
-print(farm_animals)
+# farm_animals = {"sheep", "cow", "hen"}
+# print(farm_animals)
+#
+# for animal in farm_animals:
+#     print(animal)
+#
+# print("="*48)
+#
+# wild_animals = set(["lion", "tiger", "panther", "elephant"])
+# print(wild_animals)
+#
+# for animal in wild_animals:
+#     print(animal)
+#
+# farm_animals.add("horse")
+# wild_animals.add("horse")
+# print(wild_animals)
+# print(farm_animals)
+# empty_set = set()
+# empty_set_2 = {}
+# empty_set.add("a")
+#
+# even = set(range(0, 40, 2))
+# print(even)
 
-for animal in farm_animals:
-    print(animal)
+even = set(range(0, 40, 2))
+print(even)
+print(len(even))
 
-print("="*48)
+squares_tuple = (4, 6, 8, 9, 16, 25)
+squares = set(squares_tuple)
+print(squares)
+print(len(squares))
 
-wild_animals = set(["lion", "tiger", "panther", "elephant"])
-print(wild_animals)
-
-for animal in wild_animals:
-    print(animal)
-
-farm_animals.add("horse")
-wild_animals.add("horse")
-print(wild_animals)
-print(farm_animals)
+print(even.union(squares))
+print(len(even.union(squares)))
+print("-" * 40)
+print(even.intersection(squares))
+print(even & squares)
+print(squares.intersection(even))
+print(squares & even)
